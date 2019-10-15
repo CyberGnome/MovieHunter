840b45d48033fe00e39d43c4a329d7fe5f47383b
Kachanov <kachanov_anton_1997@inbox.ru>
2019-10-15 21:06:09 +0300

    simple auth

--- server/app/api/users/users.py
+++ server/app/api/users/users.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
-from flask import jsonify, request, abort, url_for
+import datetime
+from functools import wraps
+
+import jwt
+from flask import jsonify, request, abort, g, make_response
+from werkzeug.security import check_password_hash
 
 from app import app, db
 from app.models import User, Role
@@ -9,17 +14,17 @@ from app.models import User, Role
 def new_user():
     if not request.json:
         return jsonify(
-            {'error': "Bad Request"}), 400
+            {'message': "Bad Request"}), 400
 
     username = request.json.get('username')
     password = request.json.get('password')
     if username is None or password is None:
         return jsonify(
-            {'error': "Bad Request"}), 400
+            {'message': "Bad Request"}), 400
 
     if User.query.filter_by(username=username).first() is not None:
         return jsonify(
-            {'error': "User with this login already exists!"}), 400
+            {'message': "User with this login already exists!"}), 400
 
     user = User(username=username)
     user.hash_password(password)
@@ -27,17 +32,91 @@ def new_user():
     role = Role.query.filter_by(name=Role.RoleNames.USER).first()
     if role is None:
         return jsonify(
-            {'error': "There is no user role in the database!"}), 500
+            {'message': "There is no user role in the database!"}), 500
 
     user.role.append(role)
     db.session.add(user)
     db.session.commit()
 
     return jsonify(
-        {'username': user.username}), 201
+        {'username': user.username,
+         'message': "New user created"}), 201
+
+
+@app.route('/api/users/<public_id>/', methods=['GET'])
+def get_user_by_id(public_id):
+    user = User.query.filter_by(public_id=public_id).first()
+    if not user:
+        return jsonify(
+            {'message': "User not found!"})
+
+    return jsonify(
+            {'username': user.username,
+             'created_date': user.created,
+             'public_id': user.public_id,
+             'user_roles': [{'id': x.id, 'role-name': str(x.name)} for x in Role.query.filter(Role.users.any(id=user.id))]})
+
+
+@app.route('/api/users/<public_id>/', methods=['DELETE'])
+def delete_user_by_id(public_id):
+    user = User.query.filter_by(public_id=public_id).first()
+    if not user:
+        return jsonify(
+            {'message': "User not found!"})
+
+    username = user.username
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify(
+            {'message': "User was deleted!",
+             "username": username})
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query.filter_by(public_id=data['public_id']).first()
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
 
 
 @app.route('/api/users/', methods=['GET'])
-def users_list():
-    users_list = [{'id': x.id, 'username': x.username} for x in User.query.all()]
-    return jsonify({'articles-list': users_list})
+@token_required
+def get_all_users(current_user):
+    users_list = [{'id': x.public_id, 'username': x.username} for x in User.query.all()]
+    return jsonify({'users-list': users_list})
+
+
+@app.route('/api/users/login/')
+def login_user():
+    auth = request.authorization
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    user = User.query.filter_by(username=auth.username).first()
+    if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    if not user.verify_password(auth.password):
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+    token = jwt.encode({'public_id': str(user.public_id),
+                        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
+                       app.config['SECRET_KEY'])
+
+    return jsonify(
+        {'token': token.decode('UTF-8')})
