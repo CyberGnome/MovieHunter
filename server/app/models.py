61223d730bcfea809daf988e05a0a17cbeb23270
Kachanov <kachanov_anton_1997@inbox.ru>
2019-10-22 17:37:57 +0300

    added salt to user password

--- server/app/models.py
+++ server/app/models.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 import enum
+import string
 import uuid
 from datetime import datetime
+from random import random
+
 from passlib.apps import custom_app_context as pwd_context
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from itsdangerous import BadSignature, SignatureExpired
 from sqlalchemy.dialects.postgresql import UUID
 
 from app import db, app
+
+
+def salt(length=128):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
 
 
 MovieGenre = db.Table(
@@ -116,6 +122,7 @@ class User(db.Model):
     public_id = db.Column(UUID(as_uuid=True), unique=True, nullable=False, default=uuid.uuid4)
     username = db.Column(db.String(32), index=True)
     password = db.Column(db.String(128), nullable=False)
+    password_salt = db.Column(db.String(128), default=uuid.uuid5)
     created = db.Column(db.DateTime, default=datetime.utcnow)
 
     role = db.relationship('Role', secondary=UserRole, lazy='subquery',
